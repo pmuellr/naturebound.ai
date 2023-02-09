42a8525caa2085d4ef9b72ecf00f10ddef5117a8
Gidi <github@gidi.io>
2023-02-09 09:18:08 +0000

    fixed logos

--- js/creative.js
+++ js/creative.js
@@ -4,11 +4,11 @@
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-(function($) {
+(function ($) {
     "use strict"; // Start of use strict
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('a.page-scroll').bind('click', function(event) {
+    $('a.page-scroll').bind('click', function (event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: ($($anchor.attr('href')).offset().top - 50)
@@ -23,16 +23,22 @@
     })
 
     // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function() {
+    $('.navbar-collapse ul li a').click(function () {
         $('.navbar-toggle:visible').click();
     });
 
     // Fit Text Plugin for Main Header
-    $("h1").fitText(
+    $(".fit").fitText(
         1.2, {
-            minFontSize: '35px',
-            maxFontSize: '65px'
-        }
+        minFontSize: '35px',
+        maxFontSize: '65px'
+    }
+    );
+    $(".fit-small").fitText(
+        0.8, {
+        minFontSize: '15px',
+        maxFontSize: '35px'
+    }
     );
 
     // Offset for Main Navigation
